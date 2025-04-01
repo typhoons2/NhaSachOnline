bbab01e2c7a4acac783b7ee94f77151e79e8d9a9
Tran Van Duy <tranvanduy011102@gmail.com>
2025-04-01 08:37:13 +0700

    Nha Sach Online p1

---
+++ NhaSachOnline/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NhaSachOnline.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
